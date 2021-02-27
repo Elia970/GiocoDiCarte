1627f2fc575437706111e04560bd8b34f977aaa5
Elia <cesaretti440@gmail.com>
2021-02-27 10:43:37 +0100

    Gioco

---
+++ cesaretti.elia.4j.giocodicarte/cesaretti.elia.4j.giocodicarte/Models/Carta.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cesaretti.elia._4j.giocodicarte.Models
+{
+    class Carta
+    {
+        private int _valore;
+        private string _seme;
+        public int valore
+        {
+            get
+            {
+                return _valore;
+            }
+            set
+            {
+                _valore = value;
+            }
+        }
+        public string seme
+        {
+            get
+            {
+                return _seme;
+            }
+            set
+            {
+                _seme = value;
+            }
+        }
+        public Carta(int valore, string seme)
+        {
+            _valore = valore;
+            _seme = seme.ToLower();
+        }
+        public string Visualizza(Carta Carta1)
+        {
+            if (_valore == 14)
+            {
+                return "La carta è l'Asso di " + _seme;
+            }
+            else
+            {
+                return "La tua carta è il " + _valore + " di " + _seme;
+            }
+        }
+        public string Vince(Carta Carta1, Carta Carta2)
+        {
+            bool r;
+            if (Carta1._seme == Carta2._seme)
+            {
+                if (Carta1._valore > Carta2._valore)
+                {
+                    r = true;
+                }
+                else
+                {
+                    r = false;
+                }
+            }
+            else
+            {
+                if (Carta1._seme == "cuori" || Carta1._seme == "c")
+                {
+                    r = true;
+                }
+                else
+                {
+                    if (Carta2._seme == "cuori" || Carta1._seme == "c")
+                    {
+                        r = false;
+                    }
+                    else
+                    {
+                        if (Carta1._seme == "quadri" || Carta1._seme == "q")
+                        {
+                            r = true;
+                        }
+                        else
+                        {
+                            if (Carta2._seme == "quadri" || Carta2._seme == "q")
+                            {
+                                r = false;
+                            }
+                            else
+                            {
+                                if (Carta1._seme == "fiori" || Carta1._seme == "f")
+                                {
+                                    r = true;
+                                }
+                                else
+                                {
+                                    if (Carta2._seme == "fiori" || Carta2._seme == "f")
+                                    {
+                                        r = false;
+                                    }
+                                    else
+                                    {
+                                        r = false;
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            if (r)
+            {
+                return "La Prima carta è più grande della Seconda";
+            }
+            else
+            {
+                return "La Seconda carta è più grande della Prima";
+            }
+        }
+    }
+}
---
+++ cesaretti.elia.4j.giocodicarte/cesaretti.elia.4j.giocodicarte/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using cesaretti.elia._4j.giocodicarte.Models;
+
+namespace cesaretti.elia._4j.giocodicarte
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int valore1=0, valore2=0;
+            string seme1 = "", seme2 = "";
+            Console.WriteLine("Che valore ha la prima carta?");
+            valore1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Che seme ha la prima carta?");
+            seme1 = Console.ReadLine();
+            Carta Carta1 = new Carta(valore1, seme1);
+            Console.WriteLine(Carta1.Visualizza(Carta1));
+            Console.WriteLine("-----------------------------------");
+            Console.WriteLine("Che valore ha la seconda carta?");
+            valore2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Che seme ha la seconda carta?");
+            seme2 = Console.ReadLine();
+            Carta Carta2 = new Carta(valore2, seme2);
+            Console.WriteLine(Carta2.Visualizza(Carta2));
+            Console.WriteLine("-----------------------------------");
+            Console.WriteLine(Carta1.Vince(Carta1,Carta2));
+            Console.ReadLine();
+        }
+    }
+}
